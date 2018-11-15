08c25f9a1947f448edeb5878e77fbcfa4d02afac
Filip <fylyp90@hotmail.com>
2018-11-15 22:12:04 +0100

    First commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        \App\Area::creating(function ($area) {
+
+            $prefix = $area->parent ? $area->parent->name . ' ' : '';
+            $area->slug = str_slug($prefix . $area->name);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        @include('layouts.partials._head')
+    </head>
+<body>
+    <div id="app">
+        <main class="py-4">
+
+        @include('layouts.partials._navigation')
+
+        @yield('content')
+
+        </main>
+    </div>
+</body>
+</html>
